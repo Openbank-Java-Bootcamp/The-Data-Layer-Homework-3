7bba7c82914ce86a06f1f4dc722591e50ebcd16b
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2022-05-10 16:38:17 +0200

    SalesRepMenu

--- src/main/java/com/ironhack/TheDataLayerHomework3/repository/AccountRepository.java
+++ src/main/java/com/ironhack/TheDataLayerHomework3/repository/AccountRepository.java
@@ -1,9 +1,20 @@
 package com.ironhack.TheDataLayerHomework3.repository;
 
+import com.ironhack.TheDataLayerHomework3.enums.Industry;
+import com.ironhack.TheDataLayerHomework3.enums.Status;
 import com.ironhack.TheDataLayerHomework3.models.Account;
+import com.ironhack.TheDataLayerHomework3.models.Opportunity;
+import com.ironhack.TheDataLayerHomework3.models.SalesRep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository  extends JpaRepository<Account, String> {
+
+
+
+    //The mean number of Opportunities associated with an Account can be displayed by typing “Mean Opps per Account”
+    //The median number of Opportunities associated with an Account can be displayed by typing “Median Opps per Account”
+    //The maximum number of Opportunities associated with an Account can be displayed by typing “Max Opps per Account”
+    //The minimum number of Opportunities associated with an Account can be displayed by typing “Min Opps per Account”
 }
