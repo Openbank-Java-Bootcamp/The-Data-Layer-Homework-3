db8179178173b0889ca9d37661365aa3c3b0f00f
bootmbo <italia.herrera94@gmail.com>
2022-05-13 13:43:44 +0200

    Opening, credits, order and color

--- src/main/java/com/ironhack/TheDataLayerHomework3/menu/MainMenu.java
+++ src/main/java/com/ironhack/TheDataLayerHomework3/menu/MainMenu.java
@@ -1,6 +1,7 @@
 package com.ironhack.TheDataLayerHomework3.menu;
 
 import com.ironhack.TheDataLayerHomework3.utils.Input;
+import com.ironhack.TheDataLayerHomework3.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,8 +26,11 @@ public class MainMenu {
     Input inputAutowired;
 
     public void menu() {
-        int input = 0;
+        opening();
+        credits();
 
+
+        int input = 0;
 
         while (input != 99) {
 
@@ -48,55 +52,54 @@ public class MainMenu {
 
 
 
-}
 
-//
-//    public static void startNavigation() {
-//        NavigationEnum selected = null;
-//        selected = selectNavigation();
-//
-//        switch (selected) {
-//            case NEW_LEAD -> LeadNavigation.createNewLead();
-//            case SHOW_LEADS -> LeadNavigation.showLeads();
-//            case LOOKUP_LEAD -> LeadNavigation.lookUpLeadID();
-//            case CONVERT_LEAD -> LeadNavigation.convertLead();
-//            case CHANGE_STATUS -> OpportunityNavigation.changeStatus();
-//            case NEW_SALESREP -> OpportunityNavigation.changeStatus();
-//            case EXIT -> Navigation.exit();
-//        }
-//    }
-//
-//
-//    private static void exit() {
-//        System.out.println("Bye!");
-//    }
-//
-//    private static String commandMenu() {
-//
-//        return "\n ------------------------------- " +
-//                (char) 27 + "[35m" + "\nType any of the following commands" + (char) 27+ "[39m" +
-//                "\n ------------------------------- " +
-//                (char) 27 + "[34m" + "\nNew Lead : " + (char) 27 + "[39m" + "to create a new Lead in the database " +
-//                (char) 27 + "[34m" + "\nShow Leads : " + (char) 27 + "[39m" + "to see all Leads in the database " +
-//                (char) 27 + "[34m" + "\nLookup Lead : " + (char) 27 + "[39m" + "to find a single Lead " +
-//                (char) 27 + "[34m" + "\nConvert Lead : " + (char) 27 + "[39m" + "to convert the Lead to a Contact, and create it's Opportunity and Account" +
-//                (char) 27 + "[34m" + "\nChange status : " + (char) 27 + "[39m" + "to update an Opportunity's status to closed and lost or closed and won " +
-//                (char) 27 + "[36m" + "\nExit : " + (char) 27 + "[39m" + " to close the CRM";
-//
-//    }
-//
-//    private static NavigationEnum selectNavigation() {
-//        NavigationEnum selected = null;
-//
-//        String input = Input.promptTextWithValidation(commandMenu(), List.of(Validation.COMMAND));
-//
-//        switch (input.toLowerCase().trim()) {
-//            case "new lead" -> selected = NavigationEnum.NEW_LEAD;
-//            case "show leads" -> selected = NavigationEnum.SHOW_LEADS;
-//            case "lookup lead" -> selected = NavigationEnum.LOOKUP_LEAD;
-//            case "convert lead" -> selected = NavigationEnum.CONVERT_LEAD;
-//            case "change status" -> selected = NavigationEnum.CHANGE_STATUS;
-//            case "exit" -> selected = NavigationEnum.EXIT;
-//        }
-//        return selected;
-//    }
+    public void credits(){
+        clearConsole();
+        Utils.printSeparator(30);
+        System.out.println("  LLIJava☕FORCE");
+        System.out.println("  was powered and produced with much care");
+        System.out.println("  by The LLIJ Hacker Team\n");
+        System.out.println("  Special thanks to our Mentors and Peers\n ");
+        System.out.println("  We wish you enjoy it.");
+        Utils.printSeparator(30);
+        Utils.anythingToContinue();
+    }
+
+    public void opening() {
+        clearConsole();
+        teaCup();
+        System.out.println("" +
+                        "       (((,      .(((,      /((((((/  *(((((/                                    /((((((((.   .(&@@%*     .((((((*        .#&@@%(   *((((((((*         \n" +
+                        "     (@@&       #@@%      ,*(@@@**. .##%@@@.   ....                   ....     /@@@&&&&&% *@@@&/*(&@@@   %@@&#%&@@@(  /@@@%/*/#@% .@@@@&&&&&.         \n" +
+                        "    ,@@@*      ,@@@,        &@@(       &@@(  %&%%%&@@& .@@@. .@@@/ *@%%%%@@@, .&@@@%%%%# #@@@.    ,@@@* /@@@. .(@@@, &@@%         %@@@@@@@@.          \n" +
+                        "    %@@%       &@@#        (@@&       (@@@  *&@@@@@@@/  %@@//@@%  .#@@@@@@@&  (@@&*****..@@@(     #@@@ .&@@@@@@@(   ,@@@*        *@@@*.....           \n" +
+                        "   *@@@%####( /@@@#####(./(@@@#/.((//%@@@, %@@#  (@@&   *@@@@@,  *@@@. ,&@@* .@@@*       #@@@#,*#@@@#  (@@&  .&@@%   @@@@(*/(&@. %@@&(((((.           \n" +
+                        "   (########, #########.(######(.&@@@&/     (@@%,###,    ###(     ,&@@/*##(  (##(          *%@@@%*    .###,    (###    /&@@@#,  *########( " +
+                "");
+        Utils.anythingToContinue();
+
+    }
+
+    public void teaCup() {
+
+        System.out.println("" +
+                        "                                                     ,((##(###%%%%%%%%%%###%%%#,                           \n" +
+                        "                                                ,/(#&&&&&&&&&&&@&@@@@@@@@@@@@@@&&&###/                     \n" +
+                        "                                                **@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&/*                    \n" +
+                        "                                                (#(/(/***,%@@@@@@@@@@@@@@@@@@%****//#%%####(((##           \n" +
+                        "                                                ,(#(%&/%%%%##&&#%#####%%%##&%#%#%%/&#(/*      &#(          \n" +
+                        "                                                 /#(##&%%#/&&#(&%#%,&&/#%&#/&%&&%%#%%#        @##          \n" +
+                        "                                                 (##%%#%%%(&&#(%%&%%&&##%%%#&%%#%###&.      @&(%           \n" +
+                        "                                              ,%##%%%%%%%%%%%%%%&&%%&&%%&&%%%%%%%%%#@%/%#@@#%              \n" +
+                        "                                        #%%%#%####%(&%&&&&%%&&&&&&&&&&&&&&&&&@&&@&&&&%&%%@#%(%(            \n" +
+                        "                                    ((%%#(#######%&&@&@&(@%%&@%@@@@@&&&@@&@@&@&&@%&&@%%%#######%%#/        \n" +
+                        "                                  .(%%#((((#######%#&@@@&@@&@@&&@&&@&%@@%@@&@@&@@@@%%%###########%##       \n" +
+                        "                                  .(%%%(###((((#######@@@@@@@@@@@@@@@@@@@@@@@@@@@###(#(((((((((#%%#*       \n" +
+                        "                                    #(*#((#(((((/(%@#%@@@@@@@@@@@@@@@@@@@@@@@@%&@%##((/((((/(%&(*(.        \n" +
+                        "                                        %#(//%&&((#%&%&&&&&&@@@&@@&@@@&@@@@@@&&&&&%#%/#%##*/(##            \n" +
+                        "                                              *&%%%#((((/(///#%#*/%%#/*&%(/*((/(/((#%%%&                   \n" +
+                        "                                                                ..,,,,.. \n" +
+                "");
+    }
+
+}
